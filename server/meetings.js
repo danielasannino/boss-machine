f97b11b7d0b44008743b821f91cc393b5e896555
Daniela Sannino <daniela.sannino1@gmail.com>
2021-06-10 18:00:52 -0400

    updated meetingsRouter

--- server/meetings.js
+++ server/meetings.js
@@ -2,19 +2,24 @@ const express = require('express');
 const meetingsRouter = express.Router();
 
 const {
-    createMeeting,
-    getAllFromDatabase,
-    getFromDatabaseById,
     addToDatabase,
-    updateInstanceInDatabase,
-    deleteFromDatabasebyId,
+    createMeeting,
     deleteAllFromDatabase,
-    etElementById, getIndexById, updateElement,
-    seedElements, createElement
+    getAllFromDatabase
+
 } = require('./db');
 
 meetingsRouter.get('/', (req, res, next) => {
     res.send(getAllFromDatabase('meetings'));
+});
+
+meetingsRouter.post('/', (req, res, next) => {
+    const newMeeting = addToDatabase('meetings', createMeeting());
+    res.status(201).send(newMeeting);
+});
+
+meetingsRouter.delete('/', (req, res, next) => {
+    res.status(204).send(deleteAllFromDatabase('meetings'));
 });
 
 module.exports = meetingsRouter;
