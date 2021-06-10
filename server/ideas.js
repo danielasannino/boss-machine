2b50050edb34dfa619ba3e35725e82b47a107e00
Daniela Sannino <daniela.sannino1@gmail.com>
2021-06-10 16:50:28 -0400

    added all routers

---
+++ server/ideas.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = require('../server');
+const ideasRouter = express.Router();
+
+const { createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+    etElementById, getIndexById, updateElement,
+    seedElements, createElement } = require('./db');
+
+ideasRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('ideas'));
+});
+
+module.exports = ideasRouter;
