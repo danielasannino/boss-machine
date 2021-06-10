0f52021bd8945feb99e650230bc7675e47db621e
Daniela Sannino <daniela.sannino1@gmail.com>
2021-06-10 18:33:49 -0400

    update

--- server/minions.js
+++ server/minions.js
@@ -1,6 +1,4 @@
-const express = require('express');
-const minionsRouter = express.Router();
-
+const minionsRouter = require('express').Router();
 const {
     getAllFromDatabase,
     getFromDatabaseById,
